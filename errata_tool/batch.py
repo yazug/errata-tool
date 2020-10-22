837d6d7d1236b59171e00cfaf1a6ef1073cee9df
Jon Schlueter <jschluet@redhat.com>
2020-10-22 15:14:04 -0400

    Update model for batch

--- errata_tool/batch.py
+++ errata_tool/batch.py
@@ -2,97 +2,140 @@ from errata_tool import ErrataConnector
 from errata_tool import Erratum
 
 
-class Batch(ErrataConnector):
-    """Search ET for Details based on a batch"""
+class BatchSearch(ErrataConnector):
 
-    def __init__(self, batch_name_or_id):
+    def list(self):
+        self._data = self._get('/api/v1/batches')
+
+        return self._parse_data(self._data)
+
+    def search(self, batch_id_or_name):
+        """Fetch batch data from Errata Tool API and create batch objects"""
+
+        args = {}
+        if batch_id_or_name is None or batch_id_or_name == '':
+            return None
+        else:
+            try:
+                args['id'] = int(batch_id_or_name)
+            except ValueError:
+                args['name'] = batch_id_or_name
+
+            self._data = self.get_filter('/api/v1/batches', 'filter', **args)
+
+        ret_data = self._parse_data(self._data)
+        if len(ret_data) == 1:
+            return ret_data[0]
+
+        return ret_data
+
+    def _parse_data(self, data_blob):
+
+        ret_data = []
+
+        if data_blob and 'data' in data_blob:
+
+            batch_list = data_blob['data']
+
+            for entry in batch_list:
+                batch_id = entry['id']
+                ret_data.append(Batch(batch_id, entry))
+        return ret_data
+
+
+class Batch(ErrataConnector):
+    """Encapsulate ET Details for a specific batch"""
+
+    def __init__(self, batch_name_or_id, entry=None):
         """Find batch details by batch name or batch_id
 
         :param batch_name_or_id: batch name or batch_id from ET
         """
-        self._fetch(batch_name_or_id)
+        self._fetch(batch_name_or_id, entry=entry)
 
-    def _fetch(self, batch_name_or_id):
+    def _fetch(self, batch_name_or_id, entry=None):
         """Fetch batch data from Errata Tool API and store to properties"""
 
         args = {}
-        if batch_name_or_id is None or batch_name_or_id == '':
-            self._data = self._get('/api/v1/batches')
-        else:
+        if entry is None and batch_name_or_id is not None:
             try:
                 args['id'] = int(batch_name_or_id)
             except ValueError:
                 args['name'] = batch_name_or_id
 
-            self._data = self.get_filter('/api/v1/batches', 'filter', **args)
+            data = self.get_filter('/api/v1/batches', 'filter', **args)
+            entry = data['data'][0]
 
         self._batch = {}
-        self._batch_list = []
-        if self._data and 'data' in self._data:
+        self._data = entry
 
-            self._batch_list = self._data['data']
-
-            for entry in self._batch_list:
-                batch_id = entry['id']
-                batch_data = {
-                    'id': batch_id,
-                    'batch_name': entry['attributes']['name'],
-                    'description': entry['attributes']['description'],
-                    'release_date': entry['attributes']['release_date'],
-                    'release_name': entry['relationships']['release']['name'],
-                    'errata_id_list': [],
-                }
-                errata_list = entry['relationships']['errata']
-                batch_data['errata_id_list'] = [e['id'] for e in errata_list]
-
-                self._batch[batch_id] = batch_data
-            print(self._batch_list)
-            print(self._batch)
+        if self._data:
+            self._batch_id = entry['id']
+            self._batch_name = entry['attributes']['name']
+            self._batch_description = entry['attributes']['description']
+            self._release_date = entry['attributes']['release_date']
+            self._release_name = entry['relationships']['release']['name']
+            errata_list = entry['relationships']['errata']
+            self._errata_ids = [e['id'] for e in errata_list]
 
     @property
-    def all_batch_ids(self):
-        """retrieve matching batch(es) as dict from search"""
-        return list(self._batch.keys())
+    def batch_id(self):
+        """batch id for this batch"""
+        return self._batch_id
 
     @property
-    def all_errata_ids(self):
-        """List of all Errata IDs from batch_search
+    def batch_name(self):
+        """batch name for this batch"""
+        return self._batch_name
+
+    @property
+    def batch_description(self):
+        """batch description for this batch"""
+        return self._batch_description
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def errata_ids(self):
+        """List of all Errata IDs for this batch
 
         :return: list of Errata ids
         """
-        return self._batch
+        return self._errata_ids
 
     @property
-    def all_errata(self):
-        """List of all Erratas from batch search
+    def errata(self):
+        """List of all Erratas for a given batch_id
 
         :return: list of Errata objects
         """
-
         if self._all_errata is None:
             self._all_errata = {}
 
-        for batch_id in self._batch.keys():
-            if batch_id not in self._all_errata:
-                self._all_errata[batch_id] = []
-                for errata_id in self._batch[batch_id]['errata_id_list']:
-                    errata = Erratum(errata_id=errata_id)
-                    self._all_errata[batch_id].append(errata)
+        for errata_id in self._errata_ids:
+            errata = Erratum(errata_id=errata_id)
+            self._all_errata.append(errata)
 
-        return self._all_errata[batch_id]
+        return self._all_errata
 
     def __getattr__(self, name):
         return self._data.get(name)
 
     def __repr__(self):
 
-        return 'batch(%s)' % str([b for b in self._batch.keys()])
+        return 'batch(%s)' % self._batch_id
 
     def __str__(self):
         """Convert batch object to string representation
 
         :return: batch info string
         """
-        output = str(self._data)
+        output = "Batch: {0} [{1}]\n".format(self.batch_name, self.batch_id)
+        output += "Description: {0}\n".format(self.batch_description)
+        output += "Release Date: {0}\n".format(self.release_date)
+        output += "ET Release: {0}\n".format(self.release)
+        output += "Errata IDs: {0}\n".format(str(self.errata_ids))
 
         return output
